bc17a4e18fa4b57ff1944599b369c5bada5aae72
dev_morpheus <vitordossantoscorreia@gmail.com>
2024-05-02 16:42:58 -0300

    Criacao de metodo

--- meu-primeiro-projeto-java/src/MinhaClasse.java
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -2,7 +2,15 @@ public class MinhaClasse {
     //Classe inicio
 
     public static void main(String[] args) {
-        //Criação de um corpo
-        System.out.print ("Olá Turma");
+        //Criação do Main
+        String primeiroNome = "Vitor";
+        String segundoNome = "Correia";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println (nomeCompleto);
+
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
